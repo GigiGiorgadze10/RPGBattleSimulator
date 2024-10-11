0876e4bd252fd2e2a842e7a23f5b0aba922a06bb
GigiGiorgadze10 <giorgadze.gigi2004@gmail.com>
2024-10-11 20:04:45 +0400

    edit

--- app/Models/Warrior.php
+++ app/Models/Warrior.php
@@ -6,13 +6,10 @@ class Warrior extends Character
 {
     public function attack(Character $opponent)
     {
-        // Generate random damage within a range
         $damage = rand(20, 40);
 
-        // Ensure getName() and other variables return valid values
         echo "{$this->getName()} (Warrior) strikes {$opponent->getName()} with a sword! Dealing {$damage} damage.\n";
-        
-        // Apply damage to opponent
+
         $opponent->takeDamage($damage);
     }
 }
