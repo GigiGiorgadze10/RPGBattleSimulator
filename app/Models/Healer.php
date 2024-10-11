962bddda614d227257e9610795fad2f6d30aafe6
GigiGiorgadze10 <giorgadze.gigi2004@gmail.com>
2024-10-11 20:03:36 +0400

    changes

--- app/Models/Healer.php
+++ app/Models/Healer.php
@@ -6,16 +6,13 @@ class Healer extends Character
 {
     public function attack(Character $opponent)
     {
-        // The Healer can either heal itself or attack an opponent
         if (rand(0, 1) === 0) {
-            // Healer chooses to heal
-            $healing = rand(15, 25); // Random healing amount
+            $healing = rand(15, 25); 
             $this->health += $healing;
 
             echo "{$this->getName()} (Healer) heals itself for {$healing} health. Current health: {$this->getHealth()}.\n";
         } else {
-            // Healer chooses to attack
-            $damage = rand(5, 15); // Healer does less damage
+            $damage = rand(5, 15);
             echo "{$this->getName()} (Healer) strikes {$opponent->getName()} with a light attack! Dealing {$damage} damage.\n";
             $opponent->takeDamage($damage);
         }
